1b76ceda0455fbc111d1f3ae25877043bdf93bb4
Alisson Youssf <youssfbr@gmail.com>
2021-03-17 14:40:55 -0300

    project created

---
+++ backend/src/main/java/com/github/youssfbr/aulajpa/RevaulajpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.aulajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevaulajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevaulajpaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/aulajpa/RevaulajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.aulajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevaulajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
